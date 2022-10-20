316239bd98bf4b77898b9237ea9c206af113dace
Mohab <mohab.mostafa.9511@gmail.com>
2022-10-21 01:31:33 +0200

    Intialize the repo with the model package

---
+++ src/com/model/InvoiceHeader.java
@@ -0,0 +1,14 @@
+package com.model;
+
+public class InvoiceHeader {
+
+    //fields of Invoice line
+
+    private int invoiceNum;
+
+    private String invoiceDate;
+
+    private ArrayList<InvoiceLines> lines;
+
+
+}
---
+++ src/com/model/InvoiceLine.java
@@ -0,0 +1,11 @@
+package com.model;
+
+public class InvoiceLine {
+    //fields of Invoice line
+
+    private String itemName;
+
+    private double itemPrice;
+
+    private int count;
+}
