dd174583038d462f800f1c6d532a7801a09d957c
Mohab <mohab.mostafa.9511@gmail.com>
2022-10-24 18:28:56 +0200

    Merge pull request #2 from yolandhope/master
    invFrame class in view packge finazle in the gui Task

--- src/com/model/InvoiceLine.java
+++ src/com/model/InvoiceLine.java
@@ -1,11 +1,57 @@
 package com.model;
+;
 
-public class InvoiceLine {
+public class InvoiceLine extends InvoiceHeader {
     //fields of Invoice line
 
     private String itemName;
 
+    private int invoicenum;
     private double itemPrice;
-
+    //private InvoiceHeader invoice;
     private int count;
+
+
+
+    public InvoiceLine(int num,String itemName, double itemPrice, int count){
+        super();
+
+        this.itemName=itemName;
+
+        this.itemPrice=itemPrice;
+
+        this.count=count;
+
+        this.invoicenum=num;
+
+
+    }
+
+    public double getTotal(){
+        return count* itemPrice;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public double getItemPrice() {
+        return itemPrice;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public void setItemPrice(double itemPrice) {
+        this.itemPrice = itemPrice;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
 }
